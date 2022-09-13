84c9af93f883f2e552ed6850e1dc70ac5d5defca
Adam Chamberlain <a.chamberlain04@gmail.com>
2022-09-13 18:09:17 -0400

    Passed all questions

--- index.js
+++ index.js
@@ -290,10 +290,19 @@ Use addArtist to do the following:
 5. Add the newly created object to the copied array, then return the copied array
 🌟 EXAMPLE: Invoking addArtist(artists, 'John Doe', '1988-2022', 'Full Stack Development', 'African American', 'I have a background in customer service at Big Retail Chain. I am attending BloomTech to become a Frontend Developer.') should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array, name, years, genre, nationality, bio) {
+  const newArray = [...array];
+  const newObject = {
+    name: name,
+    years: years,
+    genre: genre,
+    nationality: nationality,
+    bio: bio
+  }
+  newArray.push(newObject);
+  return newArray;
 }
-
+   console.log(addArtist(artists, "Adam Chamberlain", "1985-2022", "Hopscotch Professional", "Secret Agent Man"));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -304,8 +313,14 @@ Use lotsOfArt to do the following:
 🌟 EXAMPLE: lotsOfArt(artists) will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]
 */
 
-function lotsOfArt(/*Your Code Here*/) {
-  /*Your Code Here*/
+function lotsOfArt(artist) {
+  let artistArray = [];
+  for (let i = 0; i < artist.length; i ++){
+    if (artist[i].paintings > 100){
+        artistArray.push(artist[i].name);
+    }
+  }
+    return artistArray;
 }
 
 
@@ -320,10 +335,14 @@ Use artistInfo to do the following:
   "Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."
 */
 
-function artistInfo(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistInfo(artist, name){
+  for (let i = 0; i < artist.length; i ++){
+    if (artist[i].name === name){
+      return artist[i].bio;
+    }
+  }
 }
-
+  console.log(artistInfo(artists, 'Frida Kahlo'));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 9: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -336,10 +355,17 @@ Use artistByCountry to do the following:
 🌟 EXAMPLE: Invoking artistByCountry(artists, 'Spanish') will return: [ 'Salvador Dali', 'Pablo Picasso', 'Francisco Goya']
 */
 
-function artistByCountry(/*Your Code Here*/){
-  /*Your Code Here*/
-}
+function artistByCountry(array, nationality){
+  let filteredArray = [];
 
+    for (let i = 0; i < array.length; i ++){
+      if (array[i].nationality === nationality){
+        filteredArray.push(array[i].name);
+      }
+    }
+    return filteredArray;
+}
+ console.log(artistByCountry(artists, "Russian"));
 
 
 /* ***** END OF TASKS ***** */
